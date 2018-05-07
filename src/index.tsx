bc730d2ec253f18a89c5fe9e588fd09abed4a0a6
Hardys <hardyscc@gmail.com>
2018-05-07 11:27:12 +0800

    add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import { App } from './components/App'
@@ -12,8 +13,12 @@ const client = new ApolloClient({ uri: 'https://portfolios.now.sh/' })
 
 const WrappedApp = (
   <ApolloProvider client={client}>
-    <App />
-    <BeerListContainer />
+    <Router>
+      <React.Fragment>
+        <Route path="/" exact={true} component={App} />
+        <Route path="/beer" component={BeerListContainer} />
+      </React.Fragment>
+    </Router>
   </ApolloProvider>
 )
 

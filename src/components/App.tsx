d190f205bfd2f76a407992bc0d4e35ab091f6cf7
Hardys <hardyscc@gmail.com>
2018-06-05 11:30:59 +0800

    retain menu highlight

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { Layout, Menu } from 'antd'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Layout } from 'antd'
 import { StockList } from './stock/StockList'
 import BeerListContainer from './beer/BeerListContainer'
+import AppMenu from './AppMenu'
 
 import './App.css'
 
@@ -10,14 +11,7 @@ export default () => (
   <Router>
     <Layout>
       <Layout.Header>
-        <Menu theme="dark" mode="horizontal" className="menu-box">
-          <Menu.Item key="1">
-            <Link to="/">Stock List</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/beer">Beer List</Link>
-          </Menu.Item>
-        </Menu>
+        <AppMenu />
       </Layout.Header>
       <Layout.Content>
         <div className="content-box">
---
+++ src/components/AppMenu.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { Menu } from 'antd'
+import { RouteComponentProps } from 'react-router'
+import { Link, withRouter } from 'react-router-dom'
+
+class AppMenu extends React.Component<RouteComponentProps<any>> {
+  public render() {
+    const { location } = this.props
+    return (
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        className="menu-box"
+        selectedKeys={[location.pathname]}>
+        <Menu.Item key="/">
+          <Link to="/">Stock List</Link>
+        </Menu.Item>
+        <Menu.Item key="/beer">
+          <Link to="/beer">Beer List</Link>
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
+
+export default withRouter(AppMenu)
